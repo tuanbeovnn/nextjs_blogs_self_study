bbe57da06e33a7654e32d7f83e536fb36c787b86
Tuan Nguyen <tuan@pezzie.com>
2024-05-21 21:34:23 +0300

    updated category

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -11,7 +11,7 @@ interface PostProps {
 export const Post: React.FC<PostProps> = ({ post }) => {
     return (
         <div className="md:p-4 p-2.5 border border-[#E8E8EA] rounded-xl">
-            <Link href={`/blog/${post.id}`} className="relative block pt-[67%]">
+            <Link href={`/blog/${post?.category.name.toLowerCase}`} className="relative block pt-[67%]">
                 <img
                     className="absolute top-0 left-0 w-full h-full object-cover object-center rounded-lg"
                     src="https://source.unsplash.com/random"
@@ -20,16 +20,16 @@ export const Post: React.FC<PostProps> = ({ post }) => {
             </Link>
 
             <div className="mt-4 p-2">
-                <Badge mode="outline" label={post.category.name} />
+                <Badge mode="outline" label={post?.category.name} />
                 <Link
-                    href={`/blog/${post.id}`}
+                    href={`/blog/${post?.id}`}
                     className="md:text-2xl text-lg text-[#181A2A] font-semibold line-clamp-3 mb-5 my-4"
                 >
-                    {post.title}
+                    {post?.title}
                 </Link>
                 <Author
                     avatar="https://images2.thanhnien.vn/Uploaded/minhnguyet/2021_11_11/trieu-lo-tu-217.jpg"
-                    name={post.createdBy.name}
+                    name={post?.createdBy.name}
                     createdAt="09/11/1998"
                 />
 
