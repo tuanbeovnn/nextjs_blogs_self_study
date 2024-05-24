556a0cdeef6c55692d2bdf8ad803b534377da8d1
Tuan Nguyen <tuan@pezzie.com>
2024-05-24 17:54:10 +0300

    updated toggle show and hidden

--- src/app/blog/[blogId]/page.tsx
+++ src/app/blog/[blogId]/page.tsx
@@ -82,6 +82,24 @@ const BlogDetail = () => {
                                 </svg>
                                 <span className="sr-only">Comment settings</span>
                             </button>
+                            <div id="dropdownComment1"
+                                className="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                                <ul className="py-1 text-sm text-gray-700 dark:text-gray-200"
+                                    aria-labelledby="dropdownMenuIconHorizontalButton">
+                                    <li>
+                                        <a href="#"
+                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"
+                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Remove</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"
+                                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                         <p className="mt-2 text-gray-700 leading-6">
                             Very straight-to-point article. Really worth time reading. Thank
