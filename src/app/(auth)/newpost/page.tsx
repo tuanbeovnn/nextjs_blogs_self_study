6ba8fde5a006dee61c08ff09690ebc3563f3db52
Tuan Nguyen <tuan@pezzie.com>
2024-06-03 21:25:01 +0300

    merge from imple

--- src/app/(auth)/newpost/page.tsx
+++ src/app/(auth)/newpost/page.tsx
@@ -18,7 +18,6 @@ const schema = yup.object().shape({
     shortDescription: yup.string().required("Short Description is required").min(1, "Content must be at least 100 characters"),
     thumbnail: yup.string().required("Thumbnail is required"),
     category: yup.string().required("Category is required"),
-    tags: yup.string().required("Category is required"),
     content: yup
         .string()
         .required("Content is required")
@@ -46,6 +45,9 @@ const AddNewPost = () => {
 
     const { handleSubmit, register, formState: { errors, isSubmitting, isValid, isDirty }, reset, control } = useForm({ resolver: yupResolver(schema), mode: "onChange" });
 
+    const getBase64 = (file: File) => {
+    };
+
     const onSubmit = (values: object) => {
 
         console.log(values);
@@ -69,42 +71,33 @@ const AddNewPost = () => {
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="tags">
                             Tags
                         </label>
-                        <Controller
-                            name='tags'
-                            control={control}
-                            rules={{ required: true }}
-                            render={({ field: { value, onChange } }) => (
-                                <div className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="tags">
-                                    {tags.map((tag, index) => (
-                                        <span key={index} className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-1 rounded dark:text-blue-400 border border-blue-400">
+                        <div className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="tags">
+                            {tags.map((tag, index) => (
+                                <span key={index} className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-1 rounded dark:text-blue-400 border border-blue-400">
+                                    {tag}
+                                    <button type="button" className="inline-flex items-center p-1 ms-2 text-sm bg-transparent rounded-sm hover:bg-blue-200  dark:hover:text-blue-300" data-dismiss-target="#badge-dismiss-default" aria-label="Remove" onClick={() => removeTag(tag)}>
+                                        <svg className="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                        </svg>
+                                        <span className="sr-only">Remove badge</span>
+                                    </button>
+                                </span>
+                            ))}
+                            <div className="md:col-span-4">
+                                <select
+                                    className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
+                                    onChange={handleTagSelect}
+                                    defaultValue=""
+                                >
+                                    <option value="" disabled>Select tags</option>
+                                    {availableTags.map((tag, index) => (
+                                        <option key={index} value={tag}>
                                             {tag}
-                                            <button type="button" className="inline-flex items-center p-1 ms-2 text-sm bg-transparent rounded-sm hover:bg-blue-200  dark:hover:text-blue-300" data-dismiss-target="#badge-dismiss-default" aria-label="Remove" onClick={() => removeTag(tag)}>
-                                                <svg className="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                                </svg>
-                                                <span className="sr-only">Remove badge</span>
-                                            </button>
-                                        </span>
+                                        </option>
                                     ))}
-                                    <div className="md:col-span-4">
-                                        <select
-                                            className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-2"
-                                            onChange={onChange}
-                                            value={value}
-                                            defaultValue=""
-                                        >
-                                            <option value="" disabled>Select tags</option>
-                                            {availableTags.map((tag, index) => (
-                                                <option key={index} value={tag}>
-                                                    {tag}
-                                                </option>
-                                            ))}
-                                        </select>
-                                    </div>
-                                </div>
-                            )}
-                        />
-
+                                </select>
+                            </div>
+                        </div>
                     </div>
                     <DropdownHook
                         control={control}
@@ -115,7 +108,6 @@ const AddNewPost = () => {
                             label: category.name
                         }))}
                     />
-                    <InputHook control={control} name="thumbnail" id="thumbnail" type="file" label="Thumbnail Image" />
                     <Controller
                         name='content'
                         control={control}
@@ -131,6 +123,7 @@ const AddNewPost = () => {
                     />
                     {errors && errors.content && <span className="text-red-500 text-sm">{errors.content.message}</span>}
                     {/* <QuillEditorHook control={control} name="content" label="Content" modules={modules} onChange={handleContentChange} /> */}
+                    <InputHook control={control} name="thumbnail" id="thumbnail" type="file" label="Thumbnail Image" />
                     <div className="flex items-center justify-between">
                         <button type="submit" className="mt-3 p-3 flex items-center justify-center px-3 text-blue-500 bg-white border border-blue-500 hover:bg-blue-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded shadow-md hover:shadow-lg transition duration-300 ease-in-out">
                             Publish
