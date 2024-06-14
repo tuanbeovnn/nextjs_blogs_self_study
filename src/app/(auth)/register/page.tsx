89ba80880c92447d8622d366e3f0328b3419fb0b
Tuan Nguyen <tuan@pezzie.com>
2024-06-14 21:53:22 +0300

    updated code

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,137 +1,138 @@
 "use client";
-import React, { useEffect } from "react";
-import { useForm, Resolver } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
+import React, {useEffect} from "react";
+import {useForm, Resolver} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { useDispatch } from "react-redux";
-import { authRegister } from "@/sagas/auth/auth-slice";
+import {useDispatch} from "react-redux";
+import {authRegister} from "@/sagas/auth/auth-slice";
 import InputHook from "@/components/Input/InputHook";
 import CheckBoxHook from "@/components/checkbox/CheckBoxHook";
 // Assuming this is another custom component
 
 type FormValues = {
-  fullname: string;
-  email: string;
-  password: string;
-  confirm_password: string;
-  terms_and_conditions: boolean;
+	fullname: string;
+	email: string;
+	password: string;
+	confirm_password: string;
+	terms_and_conditions: boolean;
 };
 
 // Define the schema
 const schema = yup.object().shape({
-  fullname: yup
-    .string()
-    .required("Full Name is required")
-    .max(50, "Full Name must be at most 50 characters"),
-  email: yup.string().email().required("Email is required"),
-  password: yup.string().required("Password is required"),
-  confirm_password: yup
-    .string()
-    .oneOf([yup.ref("password")], "Passwords must match")
-    .required("Confirm Password is required"),
-  terms_and_conditions: yup
-    .boolean()
-    .required()
-    .oneOf([true], "Please accept the terms and conditions"),
+	fullname: yup
+		.string()
+		.required("Full Name is required")
+		.max(50, "Full Name must be at most 50 characters"),
+	email: yup.string().email().required("Email is required"),
+	password: yup.string().required("Password is required"),
+	confirm_password: yup
+		.string()
+		.oneOf([yup.ref("password")], "Passwords must match")
+		.required("Confirm Password is required"),
+	terms_and_conditions: yup
+		.boolean()
+		.required()
+		.oneOf([true], "Please accept the terms and conditions"),
 });
 
 function Register() {
-  const resolver: Resolver<FormValues> = yupResolver(schema);
-  const {
-    handleSubmit,
-    formState: { errors, isSubmitting, isValid },
-    reset,
-    setFocus,
-    control,
-  } = useForm<FormValues>({ resolver, mode: "onChange" });
+	const resolver: Resolver<FormValues> = yupResolver(schema);
+	const {
+		handleSubmit,
+		formState: {errors, isSubmitting, isValid},
+		reset,
+		setFocus,
+		control,
+	} = useForm<FormValues>({resolver, mode: "onChange"});
 
-  const dispatch = useDispatch();
-  const onSubmit = async (values: FormValues) => {
-    try {
-      const data = {
-        name: values.fullname,
-        email: values.email,
-        password: values.password,
-      };
-      if (isValid) {
-        dispatch(authRegister(data));
-        reset({
-          fullname: "",
-          email: "",
-          password: "",
-          confirm_password: "",
-          terms_and_conditions: false,
-        });
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const dispatch = useDispatch();
+	const onSubmit = async (values: FormValues) => {
+		try {
+			const data = {
+				name: values.fullname,
+				email: values.email,
+				password: values.password,
+			};
+			if (isValid) {
+				dispatch(authRegister(data));
+				reset({
+					fullname: "",
+					email: "",
+					password: "",
+					confirm_password: "",
+					terms_and_conditions: false,
+				});
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  useEffect(() => {
-    setFocus("fullname");
-  }, [setFocus]);
+	useEffect(() => {
+		setFocus("fullname");
+	}, [setFocus]);
 
-  return (
-    <div className="max-w-7xl mx-auto md:px-8 px-4 custom-min-height">
-      <div className="max-w-md mx-auto my-6 w-full bg-white dark:bg-heading rounded-lg shadow-inner p-6">
-        <h2 className="text-2xl font-bold text-center text-heading dark:text-white mb-4">
-          Register
-        </h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <InputHook
-            control={control}
-            name="fullname"
-            placeholder="Your Full Name"
-            id="fullname"
-            type="text"
-            label="Full Name"
-          />
-          <InputHook
-            control={control}
-            name="email"
-            placeholder="you@example.com"
-            id="email"
-            type="email"
-            label="Email"
-          />
-          <InputHook
-            control={control}
-            name="password"
-            placeholder="••••••••"
-            id="password"
-            type="password"
-            label="Password"
-          />
-          <InputHook
-            control={control}
-            name="confirm_password"
-            placeholder="••••••••"
-            id="confirm_password"
-            type="password"
-            label="Confirm Password"
-          />
-          <CheckBoxHook
-            control={control}
-            text="I agree to the terms and conditions"
-            name="terms_and_conditions"
-            id="terms_and_conditions"
-          />
-          <button
-            type="submit"
-            className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-            onClick={() => console.log("clicked")}
-          >
-            {isSubmitting ? (
-              <div className="mx-auto w-5 h-5 border-2 border-white border-t-2 border-t-transparent rounded-full animate-spin"></div>
-            ) : (
-              "Register Now"
-            )}
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		<div className="max-w-7xl mx-auto md:px-8 px-4 custom-min-height">
+			<div className="max-w-md mx-auto my-6 w-full bg-white dark:bg-heading rounded-lg shadow-inner p-6">
+				<h2 className="text-2xl font-bold text-center text-heading dark:text-white mb-4">
+					Register
+				</h2>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<InputHook
+						control={control}
+						name="fullname"
+						placeholder="Your Full Name"
+						id="fullname"
+						type="text"
+						label="Full Name"
+					/>
+					<InputHook
+						control={control}
+						name="email"
+						placeholder="you@example.com"
+						id="email"
+						type="email"
+						label="Email"
+					/>
+					<InputHook
+						control={control}
+						name="password"
+						placeholder="••••••••"
+						id="password"
+						type="password"
+						label="Password"
+					/>
+					<InputHook
+						control={control}
+						name="confirm_password"
+						placeholder="••••••••"
+						id="confirm_password"
+						type="password"
+						label="Confirm Password"
+					/>
+					<CheckBoxHook
+						control={control}
+						text="I agree to the terms and conditions"
+						name="terms_and_conditions"
+						id="terms_and_conditions"
+					/>
+					<button
+						type="submit"
+						className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+						onClick={() => console.log("clicked")}
+					>
+						{isSubmitting ? (
+							<div
+								className="mx-auto w-5 h-5 border-2 border-white border-t-2 border-t-transparent rounded-full animate-spin"></div>
+						) : (
+							"Register Now"
+						)}
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Register;
--- src/app/blog/category/[categoryList]/page.tsx
+++ src/app/blog/category/[categoryList]/page.tsx
@@ -1,90 +1,90 @@
 "use client";
-import { Ads, Author, Badge, Post } from "@/components";
+import {Ads, Author, Badge, Post} from "@/components";
 import PostItemLoading from "@/components/Loading/PostItemLoading";
-import { fetchByCategory, postFetchByCategory } from "@/sagas/post/post-slice";
-import { PostType } from "@/types";
-import { usePathname } from "next/navigation";
-import { useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { v4 } from "uuid";
+import {fetchByCategory, postFetchByCategory} from "@/sagas/post/post-slice";
+import {PostType} from "@/types";
+import {usePathname} from "next/navigation";
+import {useEffect, useRef} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {v4} from "uuid";
 
 const itemsPerPage = 6;
 
 const CategoryBlog = () => {
-  const dispatch = useDispatch();
-  const pathname = usePathname();
-  const segments = pathname.split("/");
-  const categoryName = segments[segments.length - 1];
-  const isInitialMount = useRef(true);
-  const { listCatagory, loading, listPostByCategory } = useSelector(
-    (state: any) => state.post,
-  );
+	const dispatch = useDispatch();
+	const pathname = usePathname();
+	const segments = pathname.split("/");
+	const categoryName = segments[segments.length - 1];
+	const isInitialMount = useRef(true);
+	const {listCatagory, loading, listPostByCategory} = useSelector(
+		(state: any) => state.post,
+	);
 
-  useEffect(() => {
-    dispatch(fetchByCategory());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(fetchByCategory());
+	}, [dispatch]);
 
-  useEffect(() => {
-    if (listCatagory.length > 0) {
-      const category = listCatagory.find(
-        (item: any) => item.name === categoryName,
-      );
-      if (category) {
-        dispatch(postFetchByCategory(category.id.toString()));
-      }
-    }
-  }, [categoryName, dispatch, listCatagory]);
+	useEffect(() => {
+		if (listCatagory.length > 0) {
+			const category = listCatagory.find(
+				(item: any) => item.name === categoryName,
+			);
+			if (category) {
+				dispatch(postFetchByCategory(category.id.toString()));
+			}
+		}
+	}, [categoryName, dispatch, listCatagory]);
 
-  return (
-    <div className="max-w-7xl mx-auto md:px-8 px-4">
-      <div className="py-4 text-center md:mb-12 mb-4">
-        <h1 className="text-3xl text-heading dark:text-white font-semibold uppercase">
-          {categoryName}
-        </h1>
-      </div>
-      <div
-        style={{
-          backgroundImage:
-            "linear-gradient(0deg, rgba(20, 22, 36, 0.4), rgba(20, 22, 36, 0.4)), url(https://source.unsplash.com/random)",
-        }}
-        className="relative h-[450px] w-full bg-center bg-cover rounded-xl flex items-end"
-      >
-        <div className="p-10">
-          <Badge label="Technology" />
-          <h2 className="max-w-[720px] text-4xl text-white font-semibold mt-4 mb-6">
-            The Impact of Technology on the Workplace: How Technology is
-            Changing
-          </h2>
-          <Author
-            avatar="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            name="Tracey Wilson"
-            createdAt="August 20, 2022"
-            light
-            id="null"
-          />
-        </div>
-      </div>
+	return (
+		<div className="max-w-7xl mx-auto md:px-8 px-4">
+			<div className="py-4 text-center md:mb-12 mb-4">
+				<h1 className="text-3xl text-heading dark:text-white font-semibold uppercase">
+					{categoryName}
+				</h1>
+			</div>
+			<div
+				style={{
+					backgroundImage:
+						"linear-gradient(0deg, rgba(20, 22, 36, 0.4), rgba(20, 22, 36, 0.4)), url(https://source.unsplash.com/random)",
+				}}
+				className="relative h-[450px] w-full bg-center bg-cover rounded-xl flex items-end"
+			>
+				<div className="p-10">
+					<Badge label="Technology"/>
+					<h2 className="max-w-[720px] text-4xl text-white font-semibold mt-4 mb-6">
+						The Impact of Technology on the Workplace: How Technology is
+						Changing
+					</h2>
+					<Author
+						avatar="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+						name="Tracey Wilson"
+						createdAt="August 20, 2022"
+						light
+						id="null"
+					/>
+				</div>
+			</div>
 
-      <div className="md:pt-12 pt-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-          {!loading && listPostByCategory?.length > 0 ? (
-            listPostByCategory.map((post: PostType) => (
-              <Post key={post.id} post={post} />
-            ))
-          ) : (
-            <>
-              {new Array(itemsPerPage).fill(0).map(() => (
-                <PostItemLoading key={v4()}></PostItemLoading>
-              ))}
-            </>
-          )}
-        </div>
-        <div className="md:my-20 my-8">
-          <Ads />
-        </div>
-      </div>
-    </div>
-  );
+			<div className="md:pt-12 pt-4">
+				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+					{!loading && listPostByCategory?.length > 0 ? (
+						listPostByCategory.map((post: PostType) => (
+							<Post key={post.id} post={post}/>
+						))
+					) : (
+						<>
+							{new Array(itemsPerPage).fill(0).map(() => (
+								<PostItemLoading key={v4()}></PostItemLoading>
+							))}
+						</>
+					)}
+				</div>
+				<div className="md:my-20 my-8">
+					<Ads/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CategoryBlog;
