35af62855c371d37d93808d289ac99e8a6b5000b
Nguyen Van Viet <nguyenvanviet.150204@gmail.com>
2024-06-16 17:38:06 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,72 +7,72 @@ import { PostType } from "@/types";
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 } from "uuid";
+
 const itemsPerPage = 9;
 
 function Home() {
   const { listPost, loading, totalRecordsFeed } = useSelector(
-    (state: any) => state.post
+    (state: any) => state.post,
   );
 
   const dispatch = useDispatch();
-  const isInitialMount = useRef(true);
-  const [posts, setPosts] = useState<PostType[]>([]);
   const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
-    if (isInitialMount.current) {
-      dispatch(postFetchFeed(currentPage));
-      isInitialMount.current = false;
-    } else {
-      setPosts((prevPosts) => [...prevPosts, ...listPost]);
-    }
-  }, [dispatch, currentPage, listPost]);
+    if (listPost.length === 0) dispatch(postFetchFeed(0));
+  }, [dispatch]);
 
   const handleLoadMore = () => {
-    const nextPage = currentPage + 1;
-    setCurrentPage(nextPage);
+    const newPage = currentPage + 1;
+    setCurrentPage(newPage);
+    dispatch(postFetchFeed(newPage));
   };
 
-  const allPostsLoaded = posts?.length >= totalRecordsFeed;
+  // console.log("Post", listPost);
+
+  const allPostsLoaded = listPost?.length >= totalRecordsFeed;
   return (
-    <div>
-      <Banner />
-      <div className="md:pt-20 pt-4 pb-24">
-        <div className="max-w-7xl mx-auto md:px-8 px-4">
-          <Ads />
-          <div className="md:py-20 py-4">
-            <h2 className="font-bold text-2xl text-heading dark:text-white md:mb-8 mb-4">
-              Latest Post
-            </h2>
-            {posts?.length > 0 ? (
-              <HoverEffect items={posts} />
-            ) : (
-              <>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-                  {new Array(itemsPerPage).fill(0).map(() => (
-                    <PostItemLoading key={v4()}></PostItemLoading>
-                  ))}
-                </div>
-              </>
-            )}
-            {!allPostsLoaded && (
-              <button
-                className="md:mt-8 mt-4 mx-auto px-5 py-3 flex items-center gap-3 h-12 border border-blue-500 rounded-md font-medium font-base text-blue-500"
-                onClick={handleLoadMore}
-                disabled={loading}
-              >
-                {loading ? (
-                  <div className="mx-auto w-5 h-5 border-2 border-blue-500 border-t-2 border-t-transparent rounded-full animate-spin"></div>
-                ) : (
-                  "Load more"
-                )}
-              </button>
-            )}
+    <>
+      {/*<button onClick={handleLoadMore}>click me</button>*/}
+      <div>
+        <Banner />
+        <div className="md:pt-20 pt-4 pb-24">
+          <div className="max-w-7xl mx-auto md:px-8 px-4">
+            <Ads />
+            <div className="md:py-20 py-4">
+              <h2 className="font-bold text-2xl text-heading dark:text-white md:mb-8 mb-4">
+                Latest Post
+              </h2>
+              {listPost?.length > 0 ? (
+                <HoverEffect items={listPost} />
+              ) : (
+                <>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                    {new Array(itemsPerPage).fill(0).map(() => (
+                      <PostItemLoading key={v4()}></PostItemLoading>
+                    ))}
+                  </div>
+                </>
+              )}
+              {!allPostsLoaded && (
+                <button
+                  className="md:mt-8 mt-4 mx-auto px-5 py-3 flex items-center gap-3 h-12 border border-blue-500 rounded-md font-medium font-base text-blue-500"
+                  onClick={handleLoadMore}
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <div className="mx-auto w-5 h-5 border-2 border-blue-500 border-t-2 border-t-transparent rounded-full animate-spin"></div>
+                  ) : (
+                    "Load more"
+                  )}
+                </button>
+              )}
+            </div>
+            <Ads />
           </div>
-          <Ads />
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/sagas/post/post-slice.ts
+++ src/sagas/post/post-slice.ts
@@ -1,104 +1,103 @@
-import { CategoryItemType, PostFeedResponse, PostType } from '@/types';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-
+import { CategoryItemType, PostFeedResponse, PostType } from "@/types";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface PostsState {
-    listPost: PostType[];
-    loading: boolean;
-    postDetail: PostType | {};
-    listPostByCategory: PostType[];
-    listPostByUserId: PostType[];
-    listCatagory: CategoryItemType[];
-    totalRecordsFeed: number;
-
+  listPost: PostType[];
+  loading: boolean;
+  postDetail: PostType | {};
+  listPostByCategory: PostType[];
+  listPostByUserId: PostType[];
+  listCatagory: CategoryItemType[];
+  totalRecordsFeed: number;
 }
 
 const initialState: PostsState = {
-    listPost: [],
-    loading: false,
-    postDetail: {},
-    listPostByCategory: [],
-    listPostByUserId: [],
-    listCatagory: [],
-    totalRecordsFeed: 0,
+  listPost: [],
+  loading: false,
+  postDetail: {},
+  listPostByCategory: [],
+  listPostByUserId: [],
+  listCatagory: [],
+  totalRecordsFeed: 0,
 };
 
-
 const postSlice = createSlice({
-    name: 'posts',
-    initialState,
-    reducers: {
-        postFetchFeed: (state, action: PayloadAction<number>) => {
-            state.loading = true;
-        },
-        postFetchFeedSuccess: (state, action: PayloadAction<PostFeedResponse>) => {
-            state.listPost = action.payload.records;
-            state.totalRecordsFeed = action.payload.totalRecords;
-            state.loading = false;
-        },
-        postFetchFeedFailure: (state) => {
-            state.loading = false;
-        },
-        postFetchById: (state, action: PayloadAction<string>) => {
-            state.loading = true;
-        },
-        postFetchByIdSuccess: (state, action: PayloadAction<PostType>) => {
-            state.postDetail = action.payload;
-            state.loading = false;
-        },
-        postFetchByIdFailure: (state) => {
-            state.loading = false;
-        },
-        postFetchByCategory: (state, action: PayloadAction<string>) => {
-            state.loading = true;
-        },
-        postFetchByCategorySuccess: (state, action: PayloadAction<PostType[]>) => {
-            state.listPostByCategory = action.payload;
-            state.loading = false;
-        },
-        postFetchByCategoryFailure: (state) => {
-            state.loading = false;
-        },
-        fetchByCategory: (state, action: PayloadAction) => {
-            state.loading = true;
-        },
-        fetchByCategorySuccess: (state, action: PayloadAction<CategoryItemType[]>) => {
-            state.listCatagory = action.payload;
-            state.loading = false;
-        },
-        fetchByCategoryFailure: (state) => {
-            state.loading = false;
-        },
-        postFetchByUserId: (state, action: PayloadAction<string>) => {
-            state.loading = true;
-        },
-        postFetchByUserIdSuccess: (state, action: PayloadAction<PostType[]>) => {
-            state.listPostByUserId = action.payload;
-            state.loading = false;
-        },
-        postFetchByUserIdFailure: (state) => {
-            state.loading = false;
-        },
+  name: "posts",
+  initialState,
+  reducers: {
+    postFetchFeed: (state, action: PayloadAction<number>) => {
+      state.loading = true;
     },
+    postFetchFeedSuccess: (state, action: PayloadAction<PostFeedResponse>) => {
+      state.listPost = [...state.listPost, ...action.payload.records];
+      state.totalRecordsFeed = action.payload.totalRecords;
+      state.loading = false;
+    },
+    postFetchFeedFailure: (state) => {
+      state.loading = false;
+    },
+    postFetchById: (state, action: PayloadAction<string>) => {
+      state.loading = true;
+    },
+    postFetchByIdSuccess: (state, action: PayloadAction<PostType>) => {
+      state.postDetail = action.payload;
+      state.loading = false;
+    },
+    postFetchByIdFailure: (state) => {
+      state.loading = false;
+    },
+    postFetchByCategory: (state, action: PayloadAction<string>) => {
+      state.loading = true;
+    },
+    postFetchByCategorySuccess: (state, action: PayloadAction<PostType[]>) => {
+      state.listPostByCategory = action.payload;
+      state.loading = false;
+    },
+    postFetchByCategoryFailure: (state) => {
+      state.loading = false;
+    },
+    fetchByCategory: (state, action: PayloadAction) => {
+      state.loading = true;
+    },
+    fetchByCategorySuccess: (
+      state,
+      action: PayloadAction<CategoryItemType[]>,
+    ) => {
+      state.listCatagory = action.payload;
+      state.loading = false;
+    },
+    fetchByCategoryFailure: (state) => {
+      state.loading = false;
+    },
+    postFetchByUserId: (state, action: PayloadAction<string>) => {
+      state.loading = true;
+    },
+    postFetchByUserIdSuccess: (state, action: PayloadAction<PostType[]>) => {
+      state.listPostByUserId = action.payload;
+      state.loading = false;
+    },
+    postFetchByUserIdFailure: (state) => {
+      state.loading = false;
+    },
+  },
 });
 
 export const {
-    postFetchFeed,
-    postFetchFeedSuccess,
-    postFetchFeedFailure,
-    postFetchById,
-    postFetchByIdSuccess,
-    postFetchByIdFailure,
-    postFetchByCategory,
-    postFetchByCategorySuccess,
-    postFetchByCategoryFailure,
-    fetchByCategory,
-    fetchByCategorySuccess,
-    fetchByCategoryFailure,
-    postFetchByUserId,
-    postFetchByUserIdSuccess,
-    postFetchByUserIdFailure,
+  postFetchFeed,
+  postFetchFeedSuccess,
+  postFetchFeedFailure,
+  postFetchById,
+  postFetchByIdSuccess,
+  postFetchByIdFailure,
+  postFetchByCategory,
+  postFetchByCategorySuccess,
+  postFetchByCategoryFailure,
+  fetchByCategory,
+  fetchByCategorySuccess,
+  fetchByCategoryFailure,
+  postFetchByUserId,
+  postFetchByUserIdSuccess,
+  postFetchByUserIdFailure,
 } = postSlice.actions;
 
 export default postSlice.reducer;
